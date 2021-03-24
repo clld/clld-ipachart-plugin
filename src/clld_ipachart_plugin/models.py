d090a9f659183ad01eba71b47261a3753455143d
Robert Forkel <xrotwang@googlemail.com>
2021-03-24 17:22:41 +0100

    closes #1 and closes #3

--- src/clld_ipachart_plugin/models.py
+++ src/clld_ipachart_plugin/models.py
@@ -8,13 +8,14 @@ class InventoryMixin:
     - a CLTS sound name or
     - a pair (sound name, metadata), where metadata is a `dict`.
     """
-    def make_segment(self, sound_bipa, sound_name, **_):
+    def make_segment(self, sound_bipa, sound_name, request=None, **_):
         """
         Inheriting classes should overwrite this method to customize the way segments in the IPA
         charts are rendered.
 
         :param sound_bipa: BIPA grapheme.
         :param sound_name: CLTS sound name.
+        :param request: The current request or `None`.
         :param _: `dict` with additional metadata for the segment.
         :return: A `Segment` instance.
         """
@@ -23,31 +24,32 @@ class InventoryMixin:
             sound_name=sound_name,
             href='https://clts.clld.org/parameters/{}'.format(sound_name.replace(' ', '_')))
 
-    def _make_segment(self, sound_bipa, val):
+    def _make_segment(self, sound_bipa, val, request=None):
         if isinstance(val, (list, tuple)) and len(val) == 2:
             sound_name, data = val
         else:
             sound_name, data = val, {}
-        return self.make_segment(sound_bipa, sound_name, **data)
+        return self.make_segment(sound_bipa, sound_name, request=request, **data)
 
-    @property
-    def inventory(self):
+    def inventory(self, req):
         """
         Inheriting classes may overwrite this method, if they store "raw" inventory data
         differently.
 
         :return: `list` of `Segment` instances.
         """
-        return [self._make_segment(k, v) for k, v in self.jsondata['inventory']]
+        return [self._make_segment(k, v, request=req) for k, v in self.jsondata['inventory']]
 
-    def render_inventory(self):
+    def render_inventory(self, request=None, exclude=('+',)):
         """
         Usage: In an apps' `util.py` include code like
         ```python
         def language_detail_html(context=None, request=None, **kw):
-            return context.render_inventory()
+            return context.render_inventory(request=request)
         ```
 
+        :param request: The current request.
+        :param exclude: `list` or `tuple` of graphemes (or pseudo-graphemes) to exclude.
         :return: `dict` suitable for inclusion in template context:
         - "vowels_css": CSS snippet to style the vowels trapezoid.
         - "consonants_css" CSS snippet to style the consonants table.
@@ -55,12 +57,15 @@ class InventoryMixin:
         - "consonants_html": The IPA consonants table.
         - "uncovered": `list` of `Segment` instances which didn't fit into trapezoid or table.
         """
+        inventory = self.inventory(request)
         res = {}
         d = VowelTrapezoid()
-        covered = d.fill_slots(self.inventory)
+        covered = d.fill_slots(inventory)
         res['vowels_html'], res['vowels_css'] = d.render()
         d = PulmonicConsonants()
-        covered = covered.union(d.fill_slots(self.inventory))
+        covered = covered.union(d.fill_slots(inventory))
         res['consonants_html'], res['consonants_css'] = d.render()
-        res['uncovered'] = [p for i, p in enumerate(self.inventory) if i not in covered]
+        res['uncovered'] = [
+            p for i, p in enumerate(inventory)
+            if (i not in covered) and p.sound_bipa not in exclude]
         return res
--- tests/test_models.py
+++ tests/test_models.py
@@ -21,15 +21,18 @@ def test_InventoryMixin_custom(clts, cldf):
     class Lang2(CustomModelMixin, Contribution, InventoryMixin):
         pk = Column(Integer, ForeignKey('contribution.pk'), primary_key=True)
 
-        def make_segment(self, sound_bipa, sound_name, **kw):
+        def make_segment(self, sound_bipa, sound_name, request=None, **kw):
             return Segment(
                 sound_bipa=sound_bipa,
                 sound_name=sound_name,
-                href='http://example.com/{}'.format(kw['path']))
+                href='http://example.com/{}/{}'.format(kw['path'], request))
 
     lang = Lang2()
     load_inventories(cldf, clts, {'1': lang})
     lang.update_jsondata(
         inventory=[(k, (v, dict(path='x'))) for k, v in lang.jsondata['inventory']])
-    ctx = lang.render_inventory()
-    assert 'http://example.com/x' in ctx['vowels_html']
+    ctx = lang.render_inventory(request=5, exclude=[])
+    assert 'http://example.com/x/5' in ctx['vowels_html']
+    assert '+' in [p.sound_bipa for p in ctx['uncovered']]
+    ctx = lang.render_inventory(request=5)
+    assert '+' not in [p.sound_bipa for p in ctx['uncovered']]
